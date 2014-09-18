021edeb6fb2df581f2182b420f55d0968ae08bbc
mirko <acme@gmx.com>
2014-09-18 02:46:46 -0400

    Removed of the default implementation of the Index method with one that returns string instead of ActionResult. Additionally, implementaion for the new "Welcome" action is implemented

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -8,11 +8,23 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        //
-        // GET: /HelloWorld/
-        public ActionResult Index()
+        ////
+        //// GET: /HelloWorld/
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
+        public string Index()
         {
-            return View();
+            return "This is from within the Index method out of the HelloWorldController";
+        }
+
+
+        public string Welcome() 
+        {
+            return "This is from within he Welcome method";
         }
 	}
 }
